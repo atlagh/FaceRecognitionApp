f2dff8ea2158caf1a87d5343d6ee99e537b247da
amine <atlagh.amine@gmail.com>
2020-02-29 17:13:33 +0100

    Updates

---
+++ precache-manifest.8bfaa763fa2b48e52de7adb2eb257f6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ead2a95a3611c27fc5ded671236cfff5",
+    "url": "/FAceRecognitionApp/index.html"
+  },
+  {
+    "revision": "927d23c62b0a9289d137",
+    "url": "/FAceRecognitionApp/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c87d17a6cc10bbbb5988",
+    "url": "/FAceRecognitionApp/static/css/main.3f11801b.chunk.css"
+  },
+  {
+    "revision": "927d23c62b0a9289d137",
+    "url": "/FAceRecognitionApp/static/js/2.3ed37a4e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/FAceRecognitionApp/static/js/2.3ed37a4e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c87d17a6cc10bbbb5988",
+    "url": "/FAceRecognitionApp/static/js/main.d1a516fb.chunk.js"
+  },
+  {
+    "revision": "d62ccae402527c128b03",
+    "url": "/FAceRecognitionApp/static/js/runtime-main.d4b1bf80.js"
+  }
+]);
